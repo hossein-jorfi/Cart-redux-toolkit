8c036741c1db24b420fe771f961f4c05b26bdc24
hosseinjorfi <hosseinjorfi81@gmail.com>
2023-07-26 12:58:20 +0330

    sort section added

---
+++ components/modules/DextopSort.tsx
@@ -0,0 +1,32 @@
+"use client";
+import React, { useState } from "react";
+import sortSVG from "@/public/icons/Sort.svg";
+import Image from "next/image";
+
+const DextopSort = () => {
+  const [selectedItem, setSelectedItem] = useState(0);
+  const sorItems = [
+    { id: 0, title: "Popular" },
+    { id: 1, title: "Most visited" },
+    { id: 2, title: "Most expensive" },
+    { id: 3, title: "Cheapest" },
+  ];
+  return (
+    <div className="bg-stone-50 mt-4 p-2 rounded-md flex items-center space-x-4">
+      <div>
+        <Image src={sortSVG} alt="sortSVG" />
+      </div>
+      {sorItems.map((item) => (
+        <div
+          key={item.id}
+          onClick={() => setSelectedItem(item.id)}
+          className={`${selectedItem === item.id && "opacity-100"} opacity-50 cursor-pointer`}
+        >
+          {item.title}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default DextopSort;
