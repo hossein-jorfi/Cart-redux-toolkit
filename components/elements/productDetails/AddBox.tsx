eb39819580126cc8cf801b9912b8200c9c48a662
hosseinjorfi <hosseinjorfi81@gmail.com>
2023-07-30 09:49:13 +0330

    redux: cartSlice

--- components/elements/productDetails/AddBox.tsx
+++ components/elements/productDetails/AddBox.tsx
@@ -8,18 +8,33 @@ type AddBox = {
 import shopSVG from "@/public/icons/AddBox/shop.svg";
 import shieldSVG from "@/public/icons/AddBox/Vector.svg";
 import truckSVG from "@/public/icons/AddBox/truck-time.svg";
+import trashPNG from "@/public/icons/AddBox/trash.png"
+
+// Others
 import Image from "next/image";
 import { shopProduct } from "@/types/shopTypes";
-import { addProduct } from "@/redux/features/cart/cartSlice";
+import { addProduct, removeProduct } from "@/redux/features/cart/cartSlice";
+import { useEffect } from "react";
 
 const AddBox = ({ product }: AddBox) => {
   const cart = useSelector((store: any) => store.cart);
   const dispath = useDispatch();
+  const productInCart: shopProduct = cart.products.find(
+    (item: any) => item.id === product.id
+  );
+  useEffect(() => {
+    console.log(productInCart);
+  }, []);
   const clickHandler = () => {
-    console.log(product)
     dispath(addProduct(product));
-    console.log(cart)
+    console.log(cart);
   };
+
+  const removeHandler = () => {
+    dispath(removeProduct(product));
+    console.log(cart);
+  };
+
   return (
     <div className="bg-stone-100 rounded p-3 space-y-8 ml-5 mt-4">
       <div className="flex flex-col justify-between items-start text-slate-600 space-y-2">
@@ -38,12 +53,26 @@ const AddBox = ({ product }: AddBox) => {
       </div>
       <div className="space-y-2">
         <p className="text-orange-600">$ {product.price}</p>
-        <button
-          onClick={clickHandler}
-          className="py-3 w-full rounded bg-orange-500 text-white"
-        >
-          Add To Cart
-        </button>
+        {!productInCart ? (
+          <button
+            onClick={clickHandler}
+            className="py-3 w-full rounded bg-orange-500 text-white"
+          >
+            Add To Cart
+          </button>
+        ) : (
+          <button className="py-3 px-3 w-fit rounded bg-orange-500 text-white flex justify-center items-center space-x-10">
+            {productInCart.count === 1 ? (
+              <span onClick={removeHandler}>
+                <Image src={trashPNG} alt="trash" />
+              </span>
+            ) : (
+              <span onClick={removeHandler}>-</span>
+            )}
+            <span>{productInCart.count}</span>
+            <span onClick={clickHandler}>+</span>
+          </button>
+        )}
       </div>
     </div>
   );
--- redux/features/cart/cartSlice.ts
+++ redux/features/cart/cartSlice.ts
@@ -18,19 +18,34 @@ const cartSlice = createSlice({
     addProduct: (state, action) => {
       state.totalCount++;
       if (state.products.find((i) => i.id === action.payload.id)) {
-        console.log("exist");
         state.products.map((item) => {
           if (item.id === action.payload.id) {
             item.count = (item.count as number) + 1;
           }
         });
       } else {
-        console.log("no exist");
         state.products.push({ ...action.payload, count: 1 });
       }
+    },
+    removeProduct: (state, action) => {
+      const selectedItem = state.products.find(
+        (item) => item.id === action.payload.id
+      );
+      state.products.map((item) => {
+        if (item.id === action.payload.id) {
+          if ((selectedItem?.count as number) > 1) {
+            item.count = (item.count as number) - 1;
+          } else {
+            const index = state.products.findIndex(
+              (item) => item.id === action.payload.id
+            );
+            state.products.splice(index, 1);
+          }
+        }
+      });
     },
   },
 });
 
 export default cartSlice.reducer;
-export const { addProduct } = cartSlice.actions;
+export const { addProduct, removeProduct } = cartSlice.actions;
